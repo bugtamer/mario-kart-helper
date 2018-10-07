f591590d6ff6cedb3a96b29f5085c20608baf865
dbugtamer <35902230+bugtamer@users.noreply.github.com>
2018-10-07 18:38:13 +0200

    wip add method stubs to stats service

--- src/app/components/content/gliders/gliders.component.ts
+++ src/app/components/content/gliders/gliders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StatsService } from '../../../services/stats/stats.service';
 
 @Component({
   selector: 'app-gliders',
@@ -7,11 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GlidersComponent implements OnInit {
 
-  private _gliders: any = [{"name":"Super Glider","type":"G","size":null,"speed":{"ground":1,"water":1,"air":2,"antiGravity":0},"acceleration":1,"weight":1,"handling":{"ground":1,"water":0,"air":1,"antiGravity":1},"grid":1,"miniTurbo":1},{"name":"Waddle Wing","type":"G","size":null,"speed":{"ground":1,"water":1,"air":2,"antiGravity":0},"acceleration":1,"weight":1,"handling":{"ground":1,"water":0,"air":1,"antiGravity":1},"grid":1,"miniTurbo":1},{"name":"Hylian Kite","type":"G","size":null,"speed":{"ground":1,"water":1,"air":2,"antiGravity":0},"acceleration":1,"weight":1,"handling":{"ground":1,"water":0,"air":1,"antiGravity":1},"grid":1,"miniTurbo":1},{"name":"Cloud Glider","type":"G","size":null,"speed":{"ground":0,"water":1,"air":1,"antiGravity":1},"acceleration":2,"weight":0,"handling":{"ground":1,"water":0,"air":2,"antiGravity":1},"grid":1,"miniTurbo":2},{"name":"Parachute","type":"G","size":null,"speed":{"ground":0,"water":1,"air":1,"antiGravity":1},"acceleration":2,"weight":0,"handling":{"ground":1,"water":0,"air":2,"antiGravity":1},"grid":1,"miniTurbo":2},{"name":"Flower Glider","type":"G","size":null,"speed":{"ground":0,"water":1,"air":1,"antiGravity":1},"acceleration":2,"weight":0,"handling":{"ground":1,"water":0,"air":2,"antiGravity":1},"grid":1,"miniTurbo":2},{"name":"Paper Glider","type":"G","size":null,"speed":{"ground":0,"water":1,"air":1,"antiGravity":1},"acceleration":2,"weight":0,"handling":{"ground":1,"water":0,"air":2,"antiGravity":1},"grid":1,"miniTurbo":2},{"name":"Wario Wing","type":"G","size":null,"speed":{"ground":1,"water":0,"air":2,"antiGravity":1},"acceleration":1,"weight":2,"handling":{"ground":1,"water":1,"air":1,"antiGravity":0},"grid":0,"miniTurbo":1},{"name":"Plane Glider","type":"G","size":null,"speed":{"ground":1,"water":0,"air":2,"antiGravity":1},"acceleration":1,"weight":2,"handling":{"ground":1,"water":1,"air":1,"antiGravity":0},"grid":0,"miniTurbo":1},{"name":"Gold Glider","type":"G","size":null,"speed":{"ground":1,"water":0,"air":2,"antiGravity":1},"acceleration":1,"weight":2,"handling":{"ground":1,"water":1,"air":1,"antiGravity":0},"grid":0,"miniTurbo":1},{"name":"Peach Parasol","type":"G","size":null,"speed":{"ground":0,"water":0,"air":1,"antiGravity":1},"acceleration":2,"weight":1,"handling":{"ground":1,"water":1,"air":2,"antiGravity":0},"grid":0,"miniTurbo":2},{"name":"Parafoil","type":"G","size":null,"speed":{"ground":0,"water":0,"air":1,"antiGravity":1},"acceleration":2,"weight":1,"handling":{"ground":1,"water":1,"air":2,"antiGravity":0},"grid":0,"miniTurbo":2},{"name":"Bowser Kite","type":"G","size":null,"speed":{"ground":0,"water":0,"air":1,"antiGravity":1},"acceleration":2,"weight":1,"handling":{"ground":1,"water":1,"air":2,"antiGravity":0},"grid":0,"miniTurbo":2},{"name":"MKTV Parafoil","type":"G","size":null,"speed":{"ground":0,"water":0,"air":1,"antiGravity":1},"acceleration":2,"weight":1,"handling":{"ground":1,"water":1,"air":2,"antiGravity":0},"grid":0,"miniTurbo":2}];
+  private _gliders: any;
   
-  constructor() { }
+  constructor(private _statsService: StatsService) { }
 
   ngOnInit() {
+    this._gliders = this._statsService.getGliders();
   }
 
 }
