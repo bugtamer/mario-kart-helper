53c50c0873faf67e0deb8133fdfa91cdede4ea50
dbugtamer <35902230+bugtamer@users.noreply.github.com>
2018-10-31 23:25:48 +0100

    size pipe implementation

--- src/app/pipes/size/size.pipe.spec.ts
+++ src/app/pipes/size/size.pipe.spec.ts
@@ -1,8 +1,32 @@
 import { SizePipe } from './size.pipe';
+import KartFeatures from 'src/app/models/KartFeatures';
+import { DRIVERS } from 'src/app/services/stats/data/drivers';
+import DriverSize from 'src/app/models/DriverSize';
+
 
 describe('SizePipe', () => {
+
+  const pipe = new SizePipe();
+  const drivers: Array<KartFeatures> = DRIVERS;
+  let sut: Array<KartFeatures>;
+
+  
   it('create an instance', () => {
-    const pipe = new SizePipe();
     expect(pipe).toBeTruthy();
   });
+
+
+  it('only small drivers', () => {
+    sut = pipe.transform(drivers, [DriverSize.Small]);
+    for (let i=0; i < sut.length; i++)
+      expect( sut[i].size ).toEqual( DriverSize.Small );
+  });
+
+
+  it('no small drivers', () => {
+    sut = pipe.transform(drivers, [DriverSize.Medium, DriverSize.Large]);
+    for (let i=0; i < sut.length; i++)
+      expect( sut[i].size ).not.toEqual( DriverSize.Small );
+  });
+
 });
--- src/app/pipes/size/size.pipe.ts
+++ src/app/pipes/size/size.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import DriverSize from 'src/app/models/DriverSize';
+import KartFeatures from 'src/app/models/KartFeatures';
 
 @Pipe({
   name: 'size'
 })
 export class SizePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(featureList: KartFeatures[], filter: DriverSize[]): KartFeatures[] {
+    let hasMatch = (feature: KartFeatures) => ( filter.indexOf(feature.size) >= 0 );
+    return filter ? featureList.filter(hasMatch) : featureList;
   }
 
 }
